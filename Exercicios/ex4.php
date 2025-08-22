1b5719ba75860fc952aa5ee324ee127e406d7fa4
ITSRogers <rodrigosouzasp1@gmail.com>
2025-08-21 21:33:38 -0300

    ex5

--- Exercicios/ex4.php
+++ Exercicios/ex4.php
@@ -20,11 +20,19 @@
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $num1_div = $_POST['num1'];
-  $num2_div = $_POST['num2'];
-  $div = $num1_div / $num2_div;
-  echo "A divisão de $num1_div / $num2_div é $div.";
+if ($_SERVER["REQUEST_METHOD"] == "POST") 
+{
+    $num1_div = $_POST['num1'];
+    $num2_div = $_POST['num2'];
+    if ($num2_div == 0)
+    {
+        echo "Insira um número diferente de 0.";
+    }
+    else
+    {
+        $div = $num1_div / $num2_div;
+        echo "A divisão de $num1_div / $num2_div é $div.";
+    }
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
---
+++ Exercicios/ex5.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 5 - MEDIA DAS NOTAS</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Exercicio 5 - MEDIA DAS NOTAS</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="p1" class="form-label">Insira a nota da P1:</label>
+              <input type="number" id="p1" name="p1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="p2" class="form-label">Insira a nota da P2:</label>
+              <input type="text" id="p2" name="p2" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="p3" class="form-label">Insira a nota da P3:</label>
+              <input type="text" id="p3" name="p3" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD']=="POST")
+    {
+        $soma_notas = $_POST['p1'] + $_POST['p2'] + $_POST['p3'];
+        echo "<p>MÉDIA: " . $soma_notas/3 . "<p>";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>

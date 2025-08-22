4e7b498bdaa7a65a6a1a75f8b9fbdc038aa0260c
ITSRogers <rodrigosouzasp1@gmail.com>
2025-08-21 21:01:57 -0300

    ex2

--- Exercicios/ex2.php
+++ Exercicios/ex2.php
@@ -21,15 +21,10 @@
 </form>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $num1 = isset($_POST['num1']) ? (int)$_POST['num1'] : 0;
-    $num2 = isset($_POST['num2']) ? (int)$_POST['num2'] : 0;
-
-    if ($num1 < 0 || $num2 < 0) {
-        echo "<div class='alert alert-danger mt-3'>Por favor, insira números não negativos.</div>";
-    } else {
-        $subtracao = $num1 - $num2;
-        echo "<div class='alert alert-success mt-3'>A subtração de $num1 e $num2 é: <strong>$subtracao</strong></div>";
-    }
+  $num1_sub = $_POST['num1'];
+  $num2_sub = $_POST['num2'];
+  $sub = $num2_sub - $num1_sub;
+  echo "A subtração de $num2_sub - $num1_sub é $sub.";
 }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>

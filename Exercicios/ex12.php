9c942fdeaedf3df24f927a0dad876e5097c8f721
ITSRogers <rodrigosouzasp1@gmail.com>
2025-08-28 20:56:43 -0300

    ex12

--- Exercicios/ex12.php
+++ Exercicios/ex12.php
@@ -19,6 +19,13 @@
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+    $num_elevado = $_POST['base']**$_POST['expoente'];
+    echo "Resultado = $num_elevado";
+}
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>

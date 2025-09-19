aec595e8b97092d8f5798b8bd085a13eded72265
ITSRogers <rodrigosouzasp1@gmail.com>
2025-09-18 22:06:57 -0300

    ex7_2

--- cabecalho_nav.php
+++ cabecalho_nav.php
@@ -72,6 +72,7 @@
     <li><a class="dropdown-item" href="/Exercicios - 2/ex4_2.php">Exercicio 4 - REAJUSTE > R$100,00</a></li>
     <li><a class="dropdown-item" href="/Exercicios - 2/ex5_2.php">Exercicio 5 - VALOR ASSOCIADO AO MÊS</a></li>
     <li><a class="dropdown-item" href="/Exercicios - 2/ex6_2.php">Exercicio 6 - LOOP</a></li>
+    <li><a class="dropdown-item" href="/Exercicios - 2/ex7_2.php">Exercicio 7 - LOOP (SOMA)</a></li>
   </ul>
 </li>
       </ul>

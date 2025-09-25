f9fd2cb3ab702bc2505cb16a212b01cc520f619f
ITSRogers <rodrigosouzasp1@gmail.com>
2025-09-24 21:20:33 -0300

    ex5_3.php

--- cabecalho_nav.php
+++ cabecalho_nav.php
@@ -88,6 +88,7 @@
     <li><a class="dropdown-item" href="/Exercicios - 3/ex2_3.php">Exercício 2 - MAIÚSCULO/MINÚSCULO</a></li>
     <li><a class="dropdown-item" href="/Exercicios - 3/ex3_3.php">Exercício 3 - VERIFICAÇÃO DE PALAVRAS</a></li>
     <li><a class="dropdown-item" href="/Exercicios - 3/ex4_3.php">Exercício 4 - VERIFICAÇÃO DE DATA</a></li>
+    <li><a class="dropdown-item" href="/Exercicios - 3/ex5_3.php">Exercício 5 - RAIZ QUADRADA</a></li>
   </ul>
 </li>
       </ul>
